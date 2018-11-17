82622f00d7f7deb3ebbc4b8f30ce992e31040658
Bente de Bruin <Bente@MacBook-Pro-van-Bente.local>
2018-11-18 00:31:45 +0100

    update

---
+++ homework/week_3/cleandata.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import csv
+import json
+
+INPUT_CSV = "MoMAExhibitions1929to1989.csv"
+
+def organize_data():
+    read_data = pd.read_csv("MoMAExhibitions1929to1989.csv")
+    data_frame = pd.DataFrame(read_data)
+    data_frame = data_frame[['ExhibitionID', 'ExhibitionTitle', 'ExhibitionBeginDate', 'ExhibitionURL', 'DisplayName', 'Nationality', 'Gender']]
+    data_frame = data_frame.drop_duplicates(subset=['ExhibitionID'])
+    data_frame = data_frame.set_index('ExhibitionID')
+    data_frame = data_frame.dropna()
+    data_frame = data_frame[data_frame.ExhibitionTitle != 'no data']
+    data_frame = data_frame[data_frame.ExhibitionBeginDate != 'no data']
+    data_frame = data_frame[data_frame.ExhibitionURL != 'no data']
+    data_frame = data_frame[data_frame.DisplayName != 'no data']
+    data_frame = data_frame[data_frame.Nationality != 'no data']
+    data_frame = data_frame[data_frame.Gender != 'no data']
+    # print(data_frame)
+    return data_frame
+
+def toCSV(data_frame):
+    CSVoutfile = data_frame.to_csv('cleanedCSV.csv')
+
+if __name__ == '__main__':
+    data_frame = organize_data()
+    toCSV(data_frame)
