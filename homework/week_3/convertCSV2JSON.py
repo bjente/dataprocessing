01af2763bb93f94a10546783c1ac1374b9747896
Bente de Bruin <Bente@wcw-stud-145-109-4-38.wireless.uva.nl>
2018-11-19 16:14:27 +0100

    update

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -2,11 +2,7 @@ import csv
 import json
 import pandas as pd
 
-INPUT_CSV = "cleanedCSV.csv"
 
 def toJSON(INPUT_CSV):
-    read_data = pd.read_csv("cleanedCSV.csv")
+    read_data = pd.read_csv(INPUT_CSV)
     outfile = read_data.to_json('outfile.json', orient='index')
-
-if __name__ == '__main__':
-    toJSON(INPUT_CSV)
