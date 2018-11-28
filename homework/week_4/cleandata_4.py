6651996c9a94250ccd692fa6d521a7a5e0754d76
Bente de Bruin <Bente@MacBook-Pro-van-Bente.local>
2018-11-28 20:38:05 +0100

    update

--- homework/week_4/cleandata_4.py
+++ homework/week_4/cleandata_4.py
@@ -7,6 +7,7 @@ INPUT_CSV = "dataweek4.csv"
 def organize_data():
     data_frame = pd.read_csv("dataweek4.csv", encoding='utf-8')
     data_frame = data_frame[['LOCATION', 'Value']]
+    data_frame = data_frame.round(2)
     data_frame = data_frame.set_index('LOCATION')
     return data_frame
 
