dc39c787921907b18e55280d0362a112d8e0c40c
Bente de Bruin <Bente@wcw-stud-145-109-63-155.wireless.uva.nl>
2018-12-04 13:28:42 +0100

    json

--- homework/week_5/scatter.py
+++ homework/week_5/scatter.py
@@ -4,19 +4,35 @@ import json
 
 
 INPUT_CSV = "obesitas.csv"
+INPUT_CSV_2 = "tabakdata.csv"
+INPUT_CSV_3 = "alcdata.csv"
 
+countries = ["AUT", "CAN", "CZE", "FIN", "FRA", "GRC", "HUN", "ITA", "KOR", "LUX", "NLD", "POL", "PRT", "SVK", "ESP", "SWE", "TUR", "USA", "EST", "ISR", "SVN", "LVA"]
 
-def organize_data():
+def organize_data(filename):
     data_frame = pd.read_csv(INPUT_CSV, encoding='utf-8')
     data_frame = data_frame[['LOCATION', 'INDICATOR', 'SUBJECT', 'TIME', 'Value']]
+    data_frame = data_frame.loc[data_frame['LOCATION'].isin(countries)]
     data_frame = data_frame.dropna()
     data_frame = data_frame[(data_frame['TIME'] == 2014)]
     data_frame = data_frame[(data_frame['SUBJECT'] == 'SELFREPORTED')]
-    data_frame = data_frame.set_index('LOCATION')
+    data_frame = data_frame.reset_index(drop=True)
+    return data_frame
+
+def organize_data_2(filename):
+    data_frame = pd.read_csv(INPUT_CSV, encoding='utf-8')
+    data_frame = data_frame[['LOCATION', 'INDICATOR', 'SUBJECT', 'TIME', 'Value']]
+    data_frame = data_frame.loc[data_frame['LOCATION'].isin(countries)]
+    data_frame = data_frame.dropna()
+    data_frame = data_frame[(data_frame['TIME'] == 2014)]
+    data_frame = data_frame.reset_index(drop=True)
     return data_frame
 
 
-
 if __name__ == '__main__':
-    organized = organize_data()
-    organized.to_json('obesitas.json', orient='index')
+    df_obesitas = organize_data(INPUT_CSV)
+    df_obesitas.to_json("obesitas.json", orient='index')
+    df_tabak = organize_data_2(INPUT_CSV_2)
+    df_tabak.to_json("tabak.json", orient='index')
+    df_alcohol = organize_data(INPUT_CSV_3)
+    df_alcohol.to_json("alcohol.json", orient='index')
